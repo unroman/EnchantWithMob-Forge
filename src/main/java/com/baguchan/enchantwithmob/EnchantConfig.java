ede3441aec66f58de2cf060757c7fdef3ed8a45f
baguchan <shurf012@gmail.com>
2021-12-21 10:14:23 +0900

    backport config from 1.18.1

--- src/main/java/com/baguchan/enchantwithmob/EnchantConfig.java
+++ src/main/java/com/baguchan/enchantwithmob/EnchantConfig.java
@@ -19,10 +19,11 @@ public class EnchantConfig {
     public static boolean naturalSpawnEnchantedMob;
     public static boolean spawnEnchantedAnimal;
     public static boolean enchantYourSelf;
-
+    public static boolean changeSizeWhenEnchant;
     public static boolean showEnchantedMobHud;
 
     public static List<? extends String> ENCHANT_ON_SPAWN_EXCLUSION_MOBS;
+    public static List<? extends String> ALWAY_ENCHANTABLE_MOBS;
 
     static {
         Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
@@ -37,7 +38,9 @@ public class EnchantConfig {
         naturalSpawnEnchantedMob = COMMON.naturalSpawnEnchantedMob.get();
         spawnEnchantedAnimal = COMMON.spawnEnchantedAnimal.get();
         enchantYourSelf = COMMON.enchantYourSelf.get();
+        changeSizeWhenEnchant = COMMON.changeSizeWhenEnchant.get();
         ENCHANT_ON_SPAWN_EXCLUSION_MOBS = COMMON.ENCHANT_ON_SPAWN_EXCLUSION_MOBS.get();
+        ALWAY_ENCHANTABLE_MOBS = COMMON.ALWAY_ENCHANTABLE_MOBS.get();
     }
 
     public static void bakeConfigClient() {
@@ -70,7 +73,9 @@ public class EnchantConfig {
         public final ForgeConfigSpec.BooleanValue naturalSpawnEnchantedMob;
         public final ForgeConfigSpec.BooleanValue spawnEnchantedAnimal;
         public final ForgeConfigSpec.BooleanValue enchantYourSelf;
+        public final ForgeConfigSpec.BooleanValue changeSizeWhenEnchant;
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> ENCHANT_ON_SPAWN_EXCLUSION_MOBS;
+        public final ForgeConfigSpec.ConfigValue<List<? extends String>> ALWAY_ENCHANTABLE_MOBS;
 
         public Common(ForgeConfigSpec.Builder builder) {
             naturalSpawnEnchantedMob = builder
@@ -78,8 +83,12 @@ public class EnchantConfig {
                     .translation(EnchantWithMob.MODID + ".config.naturalSpawnEnchantedMob")
                     .define("Enchanted Mob can Spawn Natural", true);
             ENCHANT_ON_SPAWN_EXCLUSION_MOBS = builder
-                    .comment("Disables specific mob from receiveing enchantments on spawn. Use the full name, eg: dungeons_mobs:protection.")
+                    .comment("Disables specific mob from receiveing enchantments on spawn. Use the full name, eg: minecraft:ender_dragon.")
                     .define("enchantOnSpawnExclusionMobs", Lists.newArrayList("minecraft:wither", "minecraft:ender_dragon"));
+            ALWAY_ENCHANTABLE_MOBS = builder
+                    .comment("Allow the specific mob from alway receiveing enchantments on spawn. Use the full name, eg: minecraft:zombie.")
+                    .define("alwayEnchantableMobs", Lists.newArrayList());
+
             spawnEnchantedAnimal = builder
                     .comment("Enable the the spawning of enchanted animal mobs. [true / false]")
                     .translation(EnchantWithMob.MODID + ".config.spawnEnchantedAnimal")
@@ -88,7 +97,12 @@ public class EnchantConfig {
                     .comment("Enable enchanting yourself. [true / false]")
                     .translation(EnchantWithMob.MODID + ".config.enchantYourSelf")
                     .define("Enchant yourself", true);
+            changeSizeWhenEnchant = builder
+                    .comment("Change Size When Enchanted. [true / false]")
+                    .translation(EnchantWithMob.MODID + ".config.changeSizeWhenEnchant")
+                    .define("Change Size When Enchanted", true);
         }
     }
 
 }
+
