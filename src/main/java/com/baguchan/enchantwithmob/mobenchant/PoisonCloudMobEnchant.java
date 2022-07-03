f0b82a6b436f10fb5690a5928c1ea85799dc3a35
baguchan <shurf012@gmail.com>
2022-07-03 19:02:31 +0900

    fixed poison cloud enchant

--- src/main/java/com/baguchan/enchantwithmob/mobenchant/PoisonCloudMobEnchant.java
+++ src/main/java/com/baguchan/enchantwithmob/mobenchant/PoisonCloudMobEnchant.java
@@ -61,10 +61,11 @@ public class PoisonCloudMobEnchant extends MobEnchant {
 			MobEnchantUtils.executeIfPresent(owner, MobEnchants.POISON_CLOUD.get(), () -> {
 				if (!(projectile instanceof AbstractArrow) || !projectile.isOnGround()) {
 					AreaEffectCloud areaeffectcloud = new AreaEffectCloud(owner.level, event.getRayTraceResult().getLocation().x, event.getRayTraceResult().getLocation().y, event.getRayTraceResult().getLocation().z);
-					areaeffectcloud.setRadius(0.5F);
-					areaeffectcloud.setRadiusOnUse(-0.25F);
+					areaeffectcloud.setRadius(1.0F);
+					areaeffectcloud.setRadiusOnUse(-0.05F);
 					areaeffectcloud.setWaitTime(10);
-					areaeffectcloud.setDuration(100);
+					areaeffectcloud.setDuration(110);
+					areaeffectcloud.setOwner(owner);
 					areaeffectcloud.setRadiusPerTick(-areaeffectcloud.getRadius() / (float) areaeffectcloud.getDuration());
 
 					areaeffectcloud.addEffect(new MobEffectInstance(MobEffects.POISON, 60, 0));
