62ca926718f73cb24edeb728d3e8d6c23e3340ff
baguchan <shutux500@gmail.com>
2023-06-23 08:03:20 +0900

    posestack crash fixed #43

--- src/main/java/baguchan/enchantwithmob/event/EntitySizeEvent.java
+++ src/main/java/baguchan/enchantwithmob/event/EntitySizeEvent.java
@@ -5,18 +5,9 @@ import baguchan.enchantwithmob.EnchantWithMob;
 import baguchan.enchantwithmob.api.IEnchantCap;
 import baguchan.enchantwithmob.registry.MobEnchants;
 import baguchan.enchantwithmob.utils.MobEnchantUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.EntityModel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
-import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -54,63 +45,6 @@ public class EntitySizeEvent {
 
                 }
             }
-        }
-        ;
-    }
-
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public static void onRenderWorld(RenderLevelStageEvent event) {
-        if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_PARTICLES) {
-            Player player = Minecraft.getInstance().player;
-            float scale = player.getBbHeight() / 1.8F;
-
-            switch (Minecraft.getInstance().options.getCameraType()) {
-                case THIRD_PERSON_BACK:
-                    if (player.getBbHeight() > 1.8F || player.getBbHeight() < 1.8F)
-                        event.getPoseStack().translate(0, 0, -scale * 2);
-                    break;
-                case THIRD_PERSON_FRONT:
-                    if (player.getBbHeight() > 1.8F || player.getBbHeight() < 1.8F)
-                        event.getPoseStack().translate(0, 0, scale * 2);
-                    break;
-            }
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    @OnlyIn(Dist.CLIENT)
-    public static void onEntityRenderPre(RenderLivingEvent.Pre<LivingEntity, EntityModel<LivingEntity>> event) {
-        final LivingEntity entity = event.getEntity();
-        if (!event.isCanceled()) {
-            event.getPoseStack().pushPose();
-            settingSize = true;
-        } else {
-            settingSize = false;
-        }
-        if (entity instanceof IEnchantCap cap) {
-            if (cap.getEnchantCap().hasEnchant()) {
-                if (MobEnchantUtils.findMobEnchantFromHandler(cap.getEnchantCap().getMobEnchants(), MobEnchants.SMALL.get())) {
-                    int level = MobEnchantUtils.getMobEnchantLevelFromHandler(cap.getEnchantCap().getMobEnchants(), MobEnchants.SMALL.get());
-                    event.getPoseStack().scale(1.0F - 0.15F * level, 1.0F - 0.15F * level, 1.0F - 0.15F * level);
-                } else if (MobEnchantUtils.findMobEnchantFromHandler(cap.getEnchantCap().getMobEnchants(), MobEnchants.HUGE.get())) {
-                    int level = MobEnchantUtils.getMobEnchantLevelFromHandler(cap.getEnchantCap().getMobEnchants(), MobEnchants.HUGE.get());
-                    event.getPoseStack().scale(1.0F + 0.15F * level, 1.0F + 0.15F * level, 1.0F + 0.15F * level);
-                } else if (EnchantConfig.COMMON.changeSizeWhenEnchant.get()) {
-                    event.getPoseStack().scale(1.05F, 1.05F, 1.05F);
-                }
-
-
-            }
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    @OnlyIn(Dist.CLIENT)
-    public static void onEntityRenderPost(RenderLivingEvent.Post<LivingEntity, EntityModel<LivingEntity>> event) {
-        if (settingSize) {
-            event.getPoseStack().popPose();
-            settingSize = false;
         }
     }
 }
